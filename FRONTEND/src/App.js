b7e6f5b675bb9bac103d385cab3bde8b8700c646
Gustavo <liamgustavo2200@gmail.com>
2023-11-09 17:43:18 -0300

    commit com a parte do front end

---
+++ FRONTEND/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  <body>
+    <div className="column-container">
+     <div className="column-item"><button type="subimit">Ver Livros</button></div>
+     <div className="column-item"><button type="subimit">Adicionar Livros</button></div>
+     <div className="column-item"><button type="subimit">Excluir Livros</button></div>
+     <div className="column-item"><button type="subimit">Atualizar Livros</button></div>
+    </div>
+  </body>
+}
+
+export default App;
